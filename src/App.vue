8d875437d855ec3e45c9c27562836adb6c71607a
Roland Doda <rolanddoda2014@gmail.com>
2021-04-03 16:09:16 +0200

    Create SingleQuote component + basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SingleQuote v-for="quote of quotes" :key="quote.id" :quote="quote" />
+  </div>
 </template>
 
 <script lang="ts">
 import * as apiTo from "@/api";
+// Libraries
 import { Component, Vue } from "vue-property-decorator";
 // Types
 import { Quote } from "@/types/quote";
+// Components
+import SingleQuote from "@/components/SingleQuote.vue";
 
-@Component()
+@Component({
+  components: {
+    SingleQuote,
+  },
+})
 export default class App extends Vue {
   quotes: Quote[] = [];
 
@@ -17,3 +26,12 @@ export default class App extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#app {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 300px);
+  gap: 25px;
+  padding: 25px;
+}
+</style>
---
+++ src/components/SingleQuote.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="single-quote">
+    <blockquote>
+      <p>{{ quote.quote }}</p>
+    </blockquote>
+
+    <figcaption>—{{ quote.author }}</figcaption>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+// Types
+import { Quote } from "@/types/quote";
+
+@Component()
+export default class SingleQuote extends Vue {
+  @Prop({ required: true, type: Object }) readonly quote: Quote;
+}
+</script>
+
+<style lang="scss" scoped>
+.single-quote {
+  --size: 300px;
+
+  width: var(--size);
+  height: var(--size);
+  display: grid;
+  grid-template-rows: 1fr auto;
+  box-shadow: 0 0 5px black;
+  padding: 20px;
+
+  figcaption {
+    text-align: end;
+  }
+}
+</style>
