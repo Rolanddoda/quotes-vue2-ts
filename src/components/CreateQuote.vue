cc800c787389adddf69b02bbbf963d54efbd2546
Roland Doda <rolanddoda2014@gmail.com>
2021-04-03 21:46:49 +0200

    Create animated button

--- src/components/CreateQuote.vue
+++ src/components/CreateQuote.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="create-quote">
     <CreateOrUpdateModal v-model="showModal" mode="create" />
-    <b-button icon-right="plus" type="is-primary" @click="showModal = true">
-      New Quote |
-    </b-button>
+
+    <button class="animated-btn" @click="showModal = true">New Quote</button>
   </div>
 </template>
 
@@ -28,5 +27,39 @@ export default class CreateQuote extends Vue {
   bottom: 20px;
   right: 20px;
   z-index: 1;
+
+  .animated-btn {
+    --duration-timing: 0.4s ease-in-out;
+
+    position: relative;
+    padding: 8px 35px;
+    border: 2px solid #1c1c22;
+    background: transparent;
+    overflow: hidden;
+    transition: color var(--duration-timing);
+    cursor: pointer;
+    font-size: 16px;
+
+    &:before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background: #00c4a7;
+      transform: translateX(-100%);
+      transition: transform var(--duration-timing);
+      z-index: -1;
+    }
+
+    &:hover {
+      color: white;
+
+      &:before {
+        transform: translateX(0);
+      }
+    }
+  }
 }
 </style>
